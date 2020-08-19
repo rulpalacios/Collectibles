2d5fc5c8537c6ef8b0bcfeacdbe0505c361f8e6c
Raul Palacios <palacios.raul02@gmail.com>
2020-08-18 22:29:23 -0500

    user registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_020822) do
+ActiveRecord::Schema.define(version: 2020_08_19_030613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_020822) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
