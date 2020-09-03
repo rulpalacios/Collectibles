ec13e59997f05bce6dc14871a0c27d5c4edd55ed
Raul Palacios <palacios.raul02@gmail.com>
2020-09-02 20:48:36 -0500

    Auth module

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+
   resource :users, only: [:new, :create]
+
+  root 'welcome#index'
 end
